2ea5665c61ce2f6fc2a78ff091181288916db348
igrisdev <johanalvarezz237@gmail.com>
2024-12-17 08:38:18 -0500

    avanzando

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Geist', ...defaultTheme.fontFamily.sans],
+        mono: ['GeistMono', ...defaultTheme.fontFamily.mono],
+        gambarino: ['Gambarino', ...defaultTheme.fontFamily.sans],
+      },
       container: {
         screens: {
           xl: '896px',
