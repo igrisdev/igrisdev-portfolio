699bfb1bcdec3d029245653189a76c9fe93051af
igrisdev <johanalvarezz237@gmail.com>
2024-12-17 06:45:26 -0500

    inicio

---
+++ tailwind.config.mjs
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      container: {
+        screens: {
+          xl: '896px',
+        },
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+    ],
+  },
+}
