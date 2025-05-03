e981a8f8cb6c39320c07e4ce0eb4ef91141889eb
igrisdev <johanalvarezz237@gmail.com>
2025-05-02 19:07:40 -0500

    chore: update dependencies and improve styling
    - Removed outdated dependencies and added new ones in package.json.
    - Updated TypeScript configuration to include new paths.
    - Introduced global CSS file for Tailwind and DaisyUI theming.
    - Refactored Layout.astro to import global styles and apply font class.
    - Deleted old Tailwind configuration file and integrated styles directly into global CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
-
-import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
 
 import vercel from '@astrojs/vercel'
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   output: 'server',
   adapter: vercel(),
+  integrations: [react()],
 })
